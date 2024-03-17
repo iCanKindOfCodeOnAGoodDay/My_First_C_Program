9146afd919ef650f4999341602ada9f920c746db
Scott L. Quashen <Scottquashen.sq@gmail.com>
2024-03-16 22:58:27 -0700

    Add files via upload

---
+++ scottNew.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    char str[100];
+    int i;
+    printf("enter a value");
+    scanf("%d", &i);
+
+    printf("\nyou entered %d\n", i);
+    return 0;
+
+}
